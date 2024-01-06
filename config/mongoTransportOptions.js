24faf9c887a919571d45c8c722ecf9625c42962a
Anilroczz <anilpulkaram48@gmail.com>
2024-01-06 12:52:37 +0530

    Updated Logger System and Transports

--- config/mongoTransportOptions.js
+++ config/mongoTransportOptions.js
@@ -3,34 +3,24 @@
 require('dotenv').config();
 
 module.exports = {
-    "errorOptions": {
-        "level": "error",
-        "db": process.env.MONGO_URI,
-        "dbName": process.env.MONGO_DB,
-        "collection": "error-logs",
-        "options": {
-            "useNewUrlParser": true,
-            "useUnifiedTopology": true
+    applicationOptions: {
+        level: "debug",
+        db: process.env.MONGO_URI,
+        dbName: process.env.MONGO_DB,
+        collection: "application-logs",
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         }
     },
-    "httpOptions": {
-        "level": "http",
-        "db": process.env.MONGO_URI,
-        "dbName": process.env.MONGO_DB,
-        "collection": "http-logs",
-        "options": {
-            "useNewUrlParser": true,
-            "useUnifiedTopology": true
-        }
-    },
-    "combinedOptions": {
-        "level": "debug",
-        "db": process.env.MONGO_URI,
-        "dbName": process.env.MONGO_DB,
-        "collection": "logs",
-        "options": {
-            "useNewUrlParser": true,
-            "useUnifiedTopology": true
+    errorOptions: {
+        level: "error",
+        db: process.env.MONGO_URI,
+        dbName: process.env.MONGO_DB,
+        collection: "application-error-logs",
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         }
     }
 }
